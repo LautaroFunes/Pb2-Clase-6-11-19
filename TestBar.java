754a41fa561a6ff1e272c970e0b9abafd5614fec
LautaroFunes <55364716+LautaroFunes@users.noreply.github.com>
2019-11-06 10:15:18 -0300

    Create TestBar.java

---
+++ TestBar.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestBar {
+
+	Cliente c1 = new Cliente("Roberto1", "Rodriguez", 11111111);
+	Cliente c2 = new Cliente("Roberto2", "Rodriguez", 22222222);
+	Cliente c3 = new Cliente("Matias3", "Fernandez", 444444444);
+	Cliente c4 = new Cliente("Roberto4", "Rodriguez", 33333333);
+	Bar b1 = new Bar();
+	
+	@Test
+	public void agregarClienteDos() {
+		b1.agregarCliente(c1);
+		b1.agregarCliente(c2);
+		b1.agregarCliente(c3);
+		b1.agregarCliente(c4);
+		
+		assertEquals(b1.getClientes().size(), 4);
+		
+		for(Cliente v : b1.getClientes()) {
+			System.out.println(v);
+		}
+		
+	}
+
+}
