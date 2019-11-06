5b0acf067503b7fc956db64def07182e681a45e3
LautaroFunes <55364716+LautaroFunes@users.noreply.github.com>
2019-11-06 11:41:04 -0300

    Update Bar.java

--- Bar.java
+++ Bar.java
@@ -19,3 +19,32 @@ public class Bar {
 	}
 
 }
+
+---Despues del recreo /Orden Nombre y apellido/ --->
+	
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Bar {
+
+	Set<Cliente> clientes = new TreeSet<Cliente>();
+
+	public Bar(Comparator orden) {
+		clientes = new TreeSet<Cliente>(orden);
+	}
+
+	public void agregarCliente(Cliente cliente) {
+		clientes.add(cliente);
+	}
+
+	public Set<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(Set<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+}
